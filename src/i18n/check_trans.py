9fb6d9f56be71250feb5a1e0eed2be2a20d92681
Bogumił Kamiński <bkamins@sgh.waw.pl>
2017-01-10 09:11:38 +0100

    updated Polish translation and added check_trans.py that checks if translations are complete

---
+++ src/i18n/check_trans.py
@@ -0,0 +1,37 @@
+import json
+import glob
+
+def list_differences(a, b, path=""):
+    allok = True
+    for key in a:
+        if key in b:
+            val_a = a[key]
+            val_b = b[key]
+            if type(val_a) != type(val_b):
+                print("\twrong value: " + path + "/" + key)
+                allok = False
+            else:
+                if isinstance(val_a, dict):
+                    if not list_differences(val_a, val_b, path + "/" + key):
+                        allok = False
+        else:
+            print("\tmissing key: " + path + "/" + key)
+            allok = False
+
+    for key in b:
+        if not key in a:
+            print("\textra key:   " + path + "/" + key)
+            allok = False
+
+    return allok
+
+
+with open("en.json") as data_file:  
+    ref = json.load(data_file)
+
+for fname in glob.glob("*.json"):
+    with open(fname) as data_file:
+        test = json.load(data_file)
+        print("Testing language file:", fname)
+        print("Result:" , list_differences(ref, test), "\n")
+
