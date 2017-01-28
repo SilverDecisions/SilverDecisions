ae1c21437630438572176ab1459ffc55ad217aaf
Bogumił Kamiński <bkamins@sgh.waw.pl>
2017-01-28 21:27:34 +0100

    more options for translation checking

--- src/i18n/check_trans.py
+++ src/i18n/check_trans.py
@@ -31,16 +31,41 @@ def list_differences(a, b, path=""):
 
     return allok
 
+help = False
+fname_list = glob.glob("*.json")
 strict = False
 if len(sys.argv) > 1:
     if sys.argv[1] == "-s":
         strict = True
         print("Strict checking of translations mode\n")
+        if len(sys.argv) > 2:
+            fname_list = [sys.argv[2]]
+            print("Checking only ", fname_list[0], "\n")
+    elif sys.argv[1] == "-m":
+        print("Minimal checking of translations mode\n")
+        if len(sys.argv) > 2:
+            fname_list = [sys.argv[2]]
+            print("Checking only ", fname_list[0], "\n")
+    else:
+        help = True
+else:
+    help = True
+if len(sys.argv) > 3:
+    help = True
+
+if help:
+    print("usage: python check_trans.py -(m|s) [filename]")
+    print("one option must be chosen:")
+    print("    -m: minimal checking")
+    print("    -s: strict checking")
+    print("if [filename] is given only one file is checked")
+    print("otherwise all JSON files are checked")
+    exit(1)
 
 with open("en.json") as data_file:  
     ref = json.load(data_file)
 
-for fname in glob.glob("*.json"):
+for fname in fname_list:
     with open(fname) as data_file:
         test = json.load(data_file)
         print("Testing language file:", fname)
