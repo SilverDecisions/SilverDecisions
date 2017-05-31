9c4b31fc88cd74303235f8f877121e6bb398b56e
mwasiluk <m.wasiluk89@gmail.com>
2017-05-31 22:29:20 +0200

    #127 league table plot legend -  explanation of dominated region and improvement direction

--- src/league-table/league-table-plot.js
+++ src/league-table/league-table-plot.js
@@ -16,7 +16,7 @@ export class LeagueTablePlotConfig extends ScatterPlotConfig {
 
     showLegend = true;
     legend = {
-        width: 100,
+        width: 125,
     };
 
     // d3ColorCategory = false;
@@ -347,5 +347,69 @@ export class LeagueTablePlot extends ScatterPlot {
             .on("mouseover.emphasize", function(d){ self.emphasizeDot(d3.select(this), true) })
             .on("mouseout.emphasize", function(d){ self.emphasizeDot(d3.select(this), false) })
     }
+
+    updateLegend() {
+        super.updateLegend();
+        var plot = this.plot;
+        console.log('update legend');
+
+        let container = plot.legend.container.selectOrAppend("g.sd-additional-items")
+        let legendCells = plot.legend.container.select(".legendCells");
+        let margin = 20;
+
+        let texts = [
+            i18n.t("leagueTable.plot.legend.dominatedRegion"),
+            i18n.t("leagueTable.plot.legend.gradientArrows")
+        ];
+
+        container.attr("transform", "translate(0, "+(legendCells.node().getBBox().height+margin)+")");
+
+        container.selectAll("text").data(texts)
+            .enter().append("text")
+            .text(d=>d)
+            .attr('dy', "0")
+            .attr('x', "0")
+
+
+
+
+        container.selectAll("text").call(wrap, d=>d, this.config.legend.width);
+
+        function wrap(text, getTextData, width) {
+            text.each(function(d) {
+
+                var text = d3.select(this),
+                    words = getTextData(d).split(/\s+/).reverse(),
+                    word,
+                    line = [],
+                    lineNumber = 0,
+                    lineHeight = 1.1, // ems
+                    y = text.attr('y'),
+                    dy = parseFloat(text.attr('dy')),
+                    tspan = text.text(null).append('tspan')
+                        .attr('x', 0).attr('y', y).attr('dy', dy + 'em');
+
+                if(this.previousSibling){
+                    text.attr('y', this.previousSibling.getBBox().height+10)
+                }
+
+                while (word = words.pop()) {
+                    line.push(word);
+                    tspan.text(line.join(' '));
+                    if (tspan.node().getComputedTextLength() > width) {
+                        line.pop();
+                        tspan.text(line.join(' '));
+                        line = [word];
+                        tspan = text.append('tspan')
+                            .attr('x', 0).attr('y', y)
+                            .attr('dy', ++lineNumber * lineHeight + dy + 'em').text(word);
+                    }
+                }
+            });
+        }
+
+
+    }
+
 
 }
