055b3b9c52dbd030d3d51931677dac7278e684fe
mwasiluk <m.wasiluk89@gmail.com>
2017-06-11 00:21:06 +0200

    updated build, version changed to 0.8.1

--- docs/app/gen/silverdecisions-job-worker-0.8.1.js
+++ docs/app/gen/silverdecisions-job-worker-0.8.1.js
@@ -1,4 +1,4 @@
-importScripts('./silver-decisions-core-0.8.0.min.js');
+importScripts('./silver-decisions-core-0.8.1.min.js');
 var computations = require('sd-computations');
 var engine = new computations.ComputationsEngine({
     logLevel: 'warn'
