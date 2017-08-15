082e146c12b6b8e4a92d1a6a97b8a9cf2aaf6a70
mwasiluk <m@wasiluk.io>
2017-08-15 11:49:52 +0200

    #151 scope label in code definition dialog

--- src/definitions-dialog.js
+++ src/definitions-dialog.js
@@ -3,6 +3,8 @@ import {Dialog} from './dialog'
 import {Utils} from "sd-utils";
 import {AppUtils} from "./app-utils";
 import {Templates} from "./templates";
+import {domain as model} from "sd-model";
+import {i18n} from "./i18n/i18n";
 
 export class DefinitionsDialog extends Dialog {
     changeCallback;
@@ -10,6 +12,9 @@ export class DefinitionsDialog extends Dialog {
     constructor(app) {
         super(app.container.select('#sd-definitions-dialog'), app);
         var self = this;
+
+        this.definitionsScopeLabel = this.container.select(".sd-definitions-dialog-scope-label");
+
         this.definitionsCode = this.container.select('textarea#sd-definitions-dialog-definitions-code').on('input', function () {
             AppUtils.updateInputClass(d3.select(this));
         });
@@ -46,6 +51,18 @@ export class DefinitionsDialog extends Dialog {
         if (!force && !this.isVisible()) {
             return;
         }
+
+        let scopeType = 'global';
+        let labelSuffix="";
+        if (this.definitionsSourceObject instanceof model.Node) {
+            scopeType = 'node';
+            let name =  this.definitionsSourceObject.name;
+            if(name){
+                labelSuffix += " ("+name+")"
+            }
+        }
+        this.definitionsScopeLabel.text(i18n.t("definitionsDialog.scope."+scopeType)+labelSuffix);
+
         this.definitionsCode.node().value = this.definitionsSourceObject.code;
         AppUtils.updateInputClass(this.definitionsCode);
         AppUtils.autoResizeTextarea(this.definitionsCode.node());
